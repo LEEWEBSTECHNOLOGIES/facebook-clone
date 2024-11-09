41f61fb302956f6f6cf7533afec91203f3fe7504
Ali <aliuajibade317@gmail.com>
2024-11-09 22:45:42 +0100

    analyse project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
+// BEM (Block Element Modifier) Naming Convention
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Let's build the Facebook Clone</h1>
+      {/* Header */}
+      {/* App Body */}
+      {/* Sidebar */}
+      {/* Feed */}
+
     </div>
   );
 }
