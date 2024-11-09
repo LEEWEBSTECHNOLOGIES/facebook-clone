155d90f330f2bb1bf2d22d7da73d6e8df4872f68
Ali <aliuajibade317@gmail.com>
2024-11-10 00:44:21 +0100

    add header__middle icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,35 @@
 import "./header.css";
 import logo from "../../images/facebook_logo.png";
+import { Group, Home, OndemandVideo, People, Search, Storefront } from "@mui/icons-material";
+
 const Header = () => {
   return (
     <div className="header">
         <div className="header__left">
             <img src={logo} alt="" />
+            <div className="header__input">
+               <Search />
+               <input type="text" placeholder="Search Facebook" />
+            </div>
         </div>
-        <div className="header__middle"></div>
+        <div className="header__middle">
+            <div className="header__option">
+                <Home />
+            </div>
+            <div className="header__option">
+                <People />
+            </div>
+            <div className="header__option">
+                <OndemandVideo />
+            </div>
+            <div className="header__option">
+                <Storefront />
+            </div>
+            <div className="header__option">
+                <Group />
+            </div>
+            
+        </div>
         <div className="header__right"></div>
     </div>
   )
