c6a4f48abb87f1d09ba0179c98ddb2f7dfea8d93
Ali <aliuajibade317@gmail.com>
2024-11-10 08:56:27 +0100

    remove firebase

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCN6O4iGNjy3EPsn_19EH3We25z7N9uT2I",
-    authDomain: "facebook-clone-e5c6c.firebaseapp.com",
-    projectId: "facebook-clone-e5c6c",
-    storageBucket: "facebook-clone-e5c6c.firebasestorage.app",
-    messagingSenderId: "809802012156",
-    appId: "1:809802012156:web:30e09c10ff726845ff0e79",
-    measurementId: "G-RPRFH456CY"
-  };
