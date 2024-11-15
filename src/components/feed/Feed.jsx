762aec8af5c072892e985b426cec566a46381d2b
Ali <aliuajibade317@gmail.com>
2024-11-15 21:13:56 +0100

    add story

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,9 +1,10 @@
+import Storyreel from "../storyreel/Storyreel";
 import "./feed.css";
 
 const Feed = () => {
   return (
     <div className="feed">
-        {/* StoryReel */}
+        <Storyreel />
         {/* MessageSender */}
     </div>
   )
---
+++ src/components/storyreel/Storyreel.jsx
@@ -0,0 +1,18 @@
+import Story from "../story/Story";
+import "./storyreel.css";
+
+const Storyreel = () => {
+  return (
+    <div className="storyReel">
+      <Story image="https://i.pinimg.com/736x/51/ec/d0/51ecd0532e8d08227b15fa65a55cf522.jpg" profileSrc="https://nmspacemuseum.org/wp-content/uploads/2019/03/Elon_Musk.jpg" title="Elon Musk" />
+      <Story image="https://i.pinimg.com/736x/51/ec/d0/51ecd0532e8d08227b15fa65a55cf522.jpg" profileSrc="https://nmspacemuseum.org/wp-content/uploads/2019/03/Elon_Musk.jpg" title="Elon Musk" />
+      <Story image="https://i.pinimg.com/736x/51/ec/d0/51ecd0532e8d08227b15fa65a55cf522.jpg" profileSrc="https://nmspacemuseum.org/wp-content/uploads/2019/03/Elon_Musk.jpg" title="Elon Musk" />
+      <Story image="https://i.pinimg.com/736x/51/ec/d0/51ecd0532e8d08227b15fa65a55cf522.jpg" profileSrc="https://nmspacemuseum.org/wp-content/uploads/2019/03/Elon_Musk.jpg" title="Elon Musk" />
+      <Story image="https://i.pinimg.com/736x/51/ec/d0/51ecd0532e8d08227b15fa65a55cf522.jpg" profileSrc="https://nmspacemuseum.org/wp-content/uploads/2019/03/Elon_Musk.jpg" title="Elon Musk" />
+      <Story image="https://i.pinimg.com/736x/51/ec/d0/51ecd0532e8d08227b15fa65a55cf522.jpg" profileSrc="https://nmspacemuseum.org/wp-content/uploads/2019/03/Elon_Musk.jpg" title="Elon Musk" />
+
+    </div>
+  )
+}
+
+export default Storyreel
