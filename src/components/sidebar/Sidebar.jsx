936b78e25f4b6adbfd1a4d33313bfe45daa088a4
Ali <aliuajibade317@gmail.com>
2024-11-15 17:24:22 +0100

    complete sidebar and sidebarrows

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,23 @@
+import { AccessTime, Chat, EmojiFlags, ExpandMoreOutlined, LocalHospital, People, Storefront, VideoLibrary } from "@mui/icons-material";
 import SidebarRow from "../sidebarrow/SidebarRow";
 import "./sidebar.css";
+// import { mdiFacebookMessenger } from "@mdi/js"
+// import user from "../../images/user.png";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-        <SidebarRow title="Pages" />
-        <SidebarRow title="Friends"/>
-        <SidebarRow />
-        <SidebarRow />
-        <SidebarRow />
+        <SidebarRow src="https://avatars.githubusercontent.com/u/29003606?v=4" title="Ali Ajibade" />
+        <SidebarRow Icon={People} title="Friends (60 online)" />
+        <SidebarRow Icon={AccessTime} title="Memories" />
+        <SidebarRow Icon={LocalHospital} title="COVID-19 Information Center" />
+        <SidebarRow Icon={EmojiFlags} title="Pages" />
+   
+        <SidebarRow Icon={Chat} title="Messenger"/>
+        {/* <SidebarRow Icon={mdiFacebookMessenger} title="Pages" /> */}
+        <SidebarRow Icon={Storefront} title="Marketplace" />
+        <SidebarRow Icon={VideoLibrary} title="Videos"/>
+        <SidebarRow  Icon={ExpandMoreOutlined} title="Marketplace" />
     </div>
   )
 }
