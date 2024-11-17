4dbf6a6753eb801b644567334c3544db9a13d135
Ali <aliuajibade317@gmail.com>
2024-11-18 00:56:52 +0100

    feed

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -2,13 +2,17 @@ import MessageSender from "../messageSender/MessageSender";
 import Post from "../post/Post";
 import Storyreel from "../storyreel/Storyreel";
 import "./feed.css";
+import imf from "../../images/imf.jpg";
 
 const Feed = () => {
   return (
     <div className="feed">
         <Storyreel />
         <MessageSender />
-        <Post />
+        <Post profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg" message="Wow, this works" timestamp="This is s timestamp" username="Mark Zuckerberg" image={imf} />
+        <Post profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg" message="Wow, this works" timestamp="This is s timestamp" username="Mark Zuckerberg" image={imf} />
+        <Post profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg" message="Wow, this works" timestamp="This is s timestamp" username="Mark Zuckerberg" image={imf} />
+        <Post profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg" message="Wow, this works" timestamp="This is s timestamp" username="Mark Zuckerberg" image={imf} />
     </div>
   )
 }
