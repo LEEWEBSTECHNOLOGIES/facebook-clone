43ecabf946f7d7af5c428bba822d0c961ef8f828
Ali <aliuajibade317@gmail.com>
2024-11-21 14:48:14 +0100

    add post icons

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -2,17 +2,17 @@ import MessageSender from "../messageSender/MessageSender";
 import Post from "../post/Post";
 import Storyreel from "../storyreel/Storyreel";
 import "./feed.css";
-import imf from "../../images/imf.jpg";
+// import imf from "../../images/imf.jpg";
 
 const Feed = () => {
   return (
     <div className="feed">
         <Storyreel />
         <MessageSender />
+        <Post profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg" message="Wow, this works" timestamp="This is s timestamp" username="Mark Zuckerberg" image="https://images.unsplash.com/photo-1731432248686-af238c870acc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+        {/* <Post profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg" message="Wow, this works" timestamp="This is s timestamp" username="Mark Zuckerberg" image={imf} />
         <Post profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg" message="Wow, this works" timestamp="This is s timestamp" username="Mark Zuckerberg" image={imf} />
-        <Post profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg" message="Wow, this works" timestamp="This is s timestamp" username="Mark Zuckerberg" image={imf} />
-        <Post profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg" message="Wow, this works" timestamp="This is s timestamp" username="Mark Zuckerberg" image={imf} />
-        <Post profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg" message="Wow, this works" timestamp="This is s timestamp" username="Mark Zuckerberg" image={imf} />
+        <Post profilePic="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg/220px-Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg" message="Wow, this works" timestamp="This is s timestamp" username="Mark Zuckerberg" image={imf} /> */}
     </div>
   )
 }
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,5 +1,6 @@
 import { Avatar } from "@mui/material";
 import "./post.css";
+import { AccountCircle, ChatBubbleOutline, ExpandMore, NearMe, ThumbUp } from "@mui/icons-material";
 
 const Post = ({profilePic, image, username, timestamp, message}) => {
   return (
@@ -16,6 +17,25 @@ const Post = ({profilePic, image, username, timestamp, message}) => {
         </div>
         <div className="post__image">
             <img src={image} alt="" />
+        </div>
+        <div className="post__options">
+            <div className="post__option">
+                <ThumbUp />
+                <p>Like</p>
+            </div>
+            <div className="post__option">
+                <ChatBubbleOutline />
+                <p>Comment</p>
+            </div>
+            <div className="post__option">
+                <NearMe />
+                <p>Share</p>
+            </div>
+            <div className="post__option">
+                <AccountCircle />
+                <ExpandMore />
+            </div>
+            
         </div>
     </div>
   )
